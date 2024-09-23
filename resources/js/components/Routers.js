d2dcda9bbd60c68d88722701c47a2f9259a5625e
Yel-git <norielfalcasantos02@gmail.com>
2024-09-23 19:51:06 +0800

    push

---
+++ resources/js/components/Routers.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Example from "./Example";
+
+export default function Routers() {
+    return {
+        <Router>
+            <Routes>
+                <Route path="/" element={<Example />} />
+            </Routes>
+        </Router>
+    };
+}
+
+if (document.getElementById("root")) {
+    ReactDOM.render(<Routers />, document.getElementById("root"));
+}
